d7ecb6a5f699a54c6eab107de33f36d1e7d0ab3c
fotios <fotios@fundalytica.com>
2020-11-11 10:29:02 +0000

    removed white lines

--- iex.py
+++ iex.py
@@ -11,16 +11,12 @@ def iex_quote(symbol, sandbox=False):
     token = iex_token(sandbox)
 
     subdomain = 'sandbox' if sandbox else 'cloud'
-
     url = f'https://{subdomain}.iexapis.com/v1/stock/{symbol}/quote?token={token}'
 
     response = requests.get(url)
-
     if(response.status_code != requests.codes.ok):
         return { "code": response.status_code }
 
     data = json.loads(response.text)
-
     data['iexcloud-messages-used'] = response.headers['iexcloud-messages-used']
-
     return data
