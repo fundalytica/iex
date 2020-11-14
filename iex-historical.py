5b3c24fe5c6d59a14aa31b998d2fa9ae02df3c34
fotios <fotios@fundalytica.com>
2020-11-14 17:02:39 +0000

    moved requests to IEX class, moved color_print to utils

--- iex-historical.py
+++ iex-historical.py
@@ -10,14 +10,11 @@ import json
 import numpy as np
 import pandas as pd
 
-from colorama import Fore, Style
+from colorama import Fore
 
 from utils import utils, stock
 
-from iex import iex_token, iex_ranges, iex_trading_days
-
-def color_print(text, color):
-    print(f'{color}{text}{Style.RESET_ALL}')
+from iex import IEX
 
 class Local:
     def __init__(self, symbol, sandbox, csv=True):
@@ -30,11 +27,11 @@ class Local:
         self.path += symbol
         self.path = f'{self.path}.{"csv" if csv else "pkl"}'
 
-        color_print(f'\n[ Local: Init ]', Fore.GREEN)
-        color_print(f'> Symbol: {symbol}, Path: {self.path}', Fore.CYAN)
+        utils.cprint(f'\n[ Local: Init ]', Fore.GREEN)
+        utils.cprint(f'> Symbol: {symbol}, Path: {self.path}', Fore.CYAN)
 
     def read(self):
-        color_print('\n[ Local: Read Historical Data ]', Fore.GREEN)
+        utils.cprint('\n[ Local: Read Historical Data ]', Fore.GREEN)
 
         try:
             if(self.csv):
@@ -47,16 +44,16 @@ class Local:
 
             print(f'\n{df}')
         except IOError as e:
-            color_print('> File Not Found', Fore.RED)
+            utils.cprint('> File Not Found', Fore.RED)
             return None
         except pd.errors.EmptyDataError as e:
-            color_print('> Empty Data', Fore.RED)
+            utils.cprint('> Empty Data', Fore.RED)
             return None
 
         return df
 
     def write(self, df):
-        color_print('\n[ Local: Write Historical Data ]', Fore.GREEN)
+        utils.cprint('\n[ Local: Write Historical Data ]', Fore.GREEN)
 
         # sort on save
         df.sort_index(inplace=True)
@@ -66,7 +63,7 @@ class Local:
         else:
             df.to_pickle(self.path)
         
-        color_print('> OK', Fore.MAGENTA)
+        utils.cprint('> OK', Fore.MAGENTA)
 
 class Integrity:
     def __init__(self, local, remote):
@@ -74,7 +71,7 @@ class Integrity:
         self.remote = remote
 
     def missing_dates(self, df):
-        color_print(f'\n[ Integrity: Missing Dates ]', Fore.GREEN)
+        utils.cprint(f'\n[ Integrity: Missing Dates ]', Fore.GREEN)
 
         # start and end dates in historical data frame
         start = pd.to_datetime(df.index[0])
@@ -82,7 +79,7 @@ class Integrity:
         days = (end - start).days + 1
         years = days / 365
         format = '%b %d, %Y'
-        color_print(f'> {start.strftime(format)} to {end.strftime(format)}, {days:,} days = {years:.1f} years', Fore.CYAN)
+        utils.cprint(f'> {start.strftime(format)} to {end.strftime(format)}, {days:,} days = {years:.1f} years', Fore.CYAN)
 
         # all dates data frame (start to end)
         range = pd.date_range(start=start, end=end)
@@ -105,10 +102,10 @@ class Integrity:
         missing_dates_df = missing_dates_df.loc[~missing_dates_df.index.isin(stock.USOtherNonTradingDates())]
 
         if(missing_dates_df.empty):
-            color_print(f'> No Missing Dates', Fore.MAGENTA)
+            utils.cprint(f'> No Missing Dates', Fore.MAGENTA)
             return
         else:
-            color_print(f'> Missing Dates', Fore.MAGENTA)
+            utils.cprint(f'> Missing Dates', Fore.MAGENTA)
             print(f'\n{missing_dates_df}')
             return missing_dates_df
 
@@ -116,7 +113,7 @@ class Integrity:
     def additional_dates(self, df):
         insertions = 0
 
-        color_print(f'\n[ Integrity: Additional, Dates ]', Fore.GREEN)
+        utils.cprint(f'\n[ Integrity: Additional, Dates ]', Fore.GREEN)
 
         # further date range
         end = pd.to_datetime(df.index[-1])
@@ -125,7 +122,7 @@ class Integrity:
         # request further days
         further = (today - next).days + 1
         if further > 0:
-            color_print(f'\nDate Range To Date (Inclusive)', Fore.YELLOW)
+            utils.cprint(f'\nDate Range To Date (Inclusive)', Fore.YELLOW)
             dtf = '%b %d, %Y'
             print(f'({next.strftime(dtf)}) to ({today.strftime(dtf)}), {further} days')
             dr = pd.date_range(start=next, end=today)
@@ -133,7 +130,7 @@ class Integrity:
 
         if(insertions > 0):
             print(f'\n{df}')
-            color_print(f'\n{insertions} Insertions', Fore.GREEN)
+            utils.cprint(f'\n{insertions} Insertions', Fore.GREEN)
 
         return insertions
 
@@ -146,90 +143,50 @@ class Integrity:
                 index = date.strftime('%Y-%m-%d')
                 df.loc[index] = close
                 insertions += 1
-                color_print(f'\n[ Integrity: Insertion ]', Fore.GREEN)
-                color_print(f'> {index} {close}', Fore.MAGENTA)
+                utils.cprint(f'\n[ Integrity: Insertion ]', Fore.GREEN)
+                utils.cprint(f'> {index} {close}', Fore.MAGENTA)
                 # save on every date because of messages used
                 self.local.write(df)
 
-        color_print(f'\n[ Integrity: Insert Complete ]', Fore.GREEN)
-        color_print(f'> {insertions} Insertions', Fore.MAGENTA)
+        utils.cprint(f'\n[ Integrity: Insert Complete ]', Fore.GREEN)
+        utils.cprint(f'> {insertions} Insertions', Fore.MAGENTA)
 
 class Remote:
     def __init__(self, symbol, sandbox):
-        self.symbol = symbol
-        self.subdomain = 'sandbox' if sandbox else 'cloud'
-        self.token = iex_token(sandbox)
-        self.weight = 2
-        color_print(f'\n[ Remote: Init ]', Fore.GREEN)
-        color_print(f'> Symbol: {symbol}, Sandbox: {sandbox}, Token: {self.token}', Fore.CYAN)
+        self.symbol = symbol        
+        self.iex = IEX(sandbox, confirm=True, verbose=True)
+        utils.cprint(f'\n[ Remote: Init ]', Fore.GREEN)
+        utils.cprint(f'> Symbol: {symbol}, Sandbox: {sandbox}', Fore.CYAN)
 
     def fetch_range(self, range):
-        color_print(f'\n[ Remote: Fetch Historical Data - Range - Adjusted Close ]', Fore.GREEN)
+        utils.cprint(f'\n[ Remote: Fetch Historical Data - Range - Adjusted Close ]', Fore.GREEN)
 
-        if(range not in iex_ranges()):
-            color_print(f'Invalid Range, {range}', Fore.RED)
-            return
+        # iex request
+        data = self.iex.request_historical_range(self.symbol, range)
+        if data is not None:
+            df = pd.json_normalize(data)
+            df = df.drop(columns=['volume', 'change', 'changePercent', 'changeOverTime'])
+            df = df.set_index('date')
 
-        days = iex_trading_days(range)
-        cost = days * self.weight
-        color_print(f'> Range {range}, Days {days:,}, Weight {self.weight}, Message Cost ~{cost:,}', Fore.CYAN)
-
-        # request confirmation
-        confirm = utils.confirm('> Proceed with IEX request?')
-        if not confirm:
-            return
-        
-        # access to Historical Prices from more than 5 years ago is only included with paid subscriptions
-        url = f'https://{self.subdomain}.iexapis.com/v1/stock/{self.symbol}/chart/{range}?token={self.token}&chartCloseOnly=true'
-        color_print(f'> {url}', Fore.CYAN)
-
-        # TODO: repeats
-        response = requests.get(url)
-        if(response.status_code != requests.codes.ok):
-            color_print(response.status_code, Fore.RED)
-            color_print(response.text, Fore.RED)
-            return
-        messages = int(response.headers["iexcloud-messages-used"])
-        color_print(f'> Messages Used {messages:,}, Days {int(messages/self.weight):,}', Fore.MAGENTA)
-        data = json.loads(response.text)
-        df = pd.json_normalize(data)
-        
-        df = df.drop(columns=['volume', 'change', 'changePercent', 'changeOverTime'])
-        df = df.set_index('date')
-
-        if not df.empty:
-            print(df)
-            return df
+            if not df.empty:
+                print(df)
+                return df
 
     def fetch_date(self, date):
-        color_print(f'\n[ Remote: Fetch Historical Data - Day - Adjusted Close ]', Fore.GREEN)
+        utils.cprint(f'\n[ Remote: Fetch Historical Data - Day - Adjusted Close ]', Fore.GREEN)
         
-        date_str = date.strftime('%Y%m%d')
+        # iex request
+        data = self.iex.request_historical_date(self.symbol, date.strftime('%Y%m%d'))
+        if data is not None:        
+            df = pd.json_normalize(data)
 
-        cost = self.weight
-        color_print(f'> Date {date_str}, Weight {self.weight}, Message Cost {cost}', Fore.CYAN)
-
-        url = f'https://{self.subdomain}.iexapis.com/v1/stock/{self.symbol}/chart/date/{date_str}?token={self.token}&chartByDay=true'
-        color_print(f'> {url}', Fore.CYAN)
-
-        # TODO repeats
-        response = requests.get(url)
-        if(response.status_code != requests.codes.ok):
-            color_print(response.status_code, Fore.RED)
-            color_print(response.text, Fore.RED)
-            return
-        messages = int(response.headers["iexcloud-messages-used"])
-        color_print(f'> Messages Used {messages:,}, Days {int(messages/self.weight):,}', Fore.MAGENTA)
-        data = json.loads(response.text)
-        df = pd.json_normalize(data)
-
-        if df.empty:
-            color_print(f'> Date {date_str}', Fore.CYAN)
-            color_print(f'> None', Fore.MAGENTA)
-        else:
-            close = df.loc[0]['close']
-            color_print(f'> Close: {close}', Fore.MAGENTA)
-            return close
+            if df.empty:
+                utils.cprint(f'> Date {date_str}', Fore.CYAN)
+                utils.cprint(f'> None', Fore.MAGENTA)
+            else:
+                close = df.loc[0]['close']
+                utils.cprint(f'> Close: {close}', Fore.MAGENTA)
+                return close
 
 def run():
     argparser = argparse.ArgumentParser(description='IEX Historical Market Data')
@@ -238,7 +195,7 @@ def run():
     args = argparser.parse_args()
 
     if not stock.valid_symbol(args.symbol):
-        color_print(f'{symbol} is not a valid stock symbol', Fore.RED)
+        utils.cprint(f'{symbol} is not a valid stock symbol', Fore.RED)
         exit()
 
     local = Local(args.symbol, args.sandbox)
@@ -267,6 +224,14 @@ def run():
     print('')
 
 run()
+
+# TODO set max to 5y do not pay for anything
+
+# TODO fetch close if you can fetch dividends and splits, that would help skip the validity check
+
+# TODO add backwards data as well as forward data
+
+# TODO rely more on yfinance since it is free and has a library
 
 # TODO: on additions, get the last df date, make a request to see if it is valid then there are no splits or dividends
 # call the method: valid, Integrity: Valid (no splits or dividends since . . .)
